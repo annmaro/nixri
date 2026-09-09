be93580c81deb0f8f586ee46ae8b2c1d180d2b7f
annmaro <anandk60440@gmail.com>
2026-09-09 19:25:49 +0530

    added beets module:   modules/programs/media/beets/default.nix

--- modules/programs/media/beets/default.nix
+++ modules/programs/media/beets/default.nix
@@ -1,25 +1,29 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   home-manager.sharedModules = [
     (_: {
-   
-    programs.beets = {
-     enable = true;
-     settings = {
-      directory = "~/Music";
-      library = "~/Data/musiclibrary.db";
-      import = {
-        copy = "no";
-      };
-      plugins = [
-        "musicbrainz"
-        "chroma"
-        "discogs"
-      ];
-    };
-  };
-};
-)];
-}
 
+      programs.beets = {
+        enable = true;
+        settings = {
+          directory = "~/Music";
+          library = "~/Data/musiclibrary.db";
+          import = {
+            copy = "no";
+          };
+          plugins = [
+            "musicbrainz"
+            "chroma"
+            "discogs"
+          ];
+        };
+      };
+    })
+  ];
+}
