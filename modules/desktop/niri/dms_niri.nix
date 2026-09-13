506f6c4c0f9623cbc756c9d4e388d3ef949bf180
annmaro <anandk60440@gmail.com>
2026-09-13 14:24:01 +0530

    removed a redundant keybind:   modules/desktop/niri/dms_niri.nix

--- modules/desktop/niri/dms_niri.nix
+++ modules/desktop/niri/dms_niri.nix
@@ -313,7 +313,6 @@ in
               "Mod+Right" { focus-column-right; }
               "Mod+H" { focus-column-left; }
               "Mod+L" { focus-column-right; }
-              "Mod+S" { spawn "sh" "-c" "niri msg action toggle-overview"; }
               "Mod+Ctrl+Left" { move-column-left; }
               "Mod+Ctrl+Right" { move-column-right; }
               
