8f7492700edd9fe0403de9dce76159dbb4cb5bb7
annmaro <anandk60440@gmail.com>
2026-09-14 20:19:51 +0530

    new file:   modules/core/agenix/default.nix 	renamed:    modules/core/git-sops/git.nix -> modules/core/agenix/git.nix 	deleted:    modules/core/git-sops/default.nix 	new file:   secrets/secrets.nix

---
+++ modules/core/agenix/default.nix
@@ -0,0 +1,50 @@
+{
+  self,
+  host,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  inherit (import "${self}/hosts/${host}/variables.nix") username;
+in
+{
+  home-manager.users.${username} =
+    { config, pkgs, ... }:
+
+    {
+
+      imports = [
+        inputs.agenix.homeManagerModules.age
+        ./git.nix
+      ];
+
+      home.packages = [
+        inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+
+      age = {
+        identityPaths = [ "${config.home.homeDirectory}/.config/agenix/key.txt" ];
+
+        secrets = {
+          "private_ssh_key" = {
+            file = ../../../secrets/private_ssh_key.age;
+            path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+            mode = "0600";
+          };
+          /*
+                "rclone_gdrive_env" = {
+              file = ../../../secrets/rclone_gdrive_env.age;
+            };
+          */
+          "git_key_id" = {
+            file = ../../../secrets/git_key_id.age;
+          };
+          "gemini_api_key" = {
+            file = ../../../secrets/gemini_api_key.age;
+          };
+        };
+      };
+    };
+}
---
+++ secrets/secrets.nix
@@ -0,0 +1,11 @@
+let
+  user1 = "age18rjgfhahe36pc9ks2g5xmxlav5p2fdkjeml48ktj3wlw3v60rv6stel0ut";
+  systems = [ ];
+in
+{
+  "private_ssh_key.age".publicKeys = [ user1 ] ++ systems;
+  "rclone_gdrive_env.age".publicKeys = [ user1 ] ++ systems;
+  "git_email.age".publicKeys = [ user1 ] ++ systems;
+  "git_key_id.age".publicKeys = [ user1 ] ++ systems;
+  "gemini_api_key.age".publicKeys = [ user1 ] ++ systems;
+}
