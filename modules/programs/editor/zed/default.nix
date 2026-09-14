42cc084eb70febd5965601dda6470fb72be89434
annmaro <anandk60440@gmail.com>
2026-09-14 23:16:18 +0530

    changed some syntax

--- modules/programs/editor/zed/default.nix
+++ modules/programs/editor/zed/default.nix
@@ -85,8 +85,8 @@
             light = "Catppuccin Latte";
           };
 
-          ui_font_size = 16;
-          buffer_font_size = 15;
+          ui_font_size = 18;
+          #buffer_font_size = 15;
           relative_line_numbers = true;
           show_whitespaces = "selection";
 
