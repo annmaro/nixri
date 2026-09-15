6de4e5c3ea837983e104d62f8b820835c3eebe81
annmaro <anandk60440@gmail.com>
2026-09-15 16:59:21 +0530

    modified the syntax

--- modules/core/agenix/default.nix
+++ modules/core/agenix/default.nix
@@ -12,9 +12,7 @@ in
 {
   home-manager.users.${username} =
     { config, pkgs, ... }:
-
     {
-
       imports = [
         inputs.agenix.homeManagerModules.default
         ./git.nix
@@ -23,6 +21,15 @@ in
       home.packages = [
         inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
       ];
+
+      home.sessionVariablesExtra = ''
+        if [ -f "${config.age.secrets.gemini_api_key.path}" ]; then
+          export GEMINI_API_KEY="$(cat "${config.age.secrets.gemini_api_key.path}")"
+        fi
+        if [ -f "${config.age.secrets.openrouter_api_key.path}" ]; then
+          export OPENROUTER_API_KEY="$(cat "${config.age.secrets.openrouter_api_key.path}")"
+        fi
+      '';
 
       age = {
         identityPaths = [ "${config.home.homeDirectory}/.config/agenix/key.txt" ];
@@ -45,15 +52,6 @@ in
           "openrouter_api_key" = {
             file = "${self}/secrets/openrouter_api_key.age";
           };
-          # Export the decrypted agenix keys for Zed to pick up on launch
-          home.sessionVariablesExtra = ''
-            if [ -f "${config.age.secrets.gemini_api_key.path}" ]; then
-              export GEMINI_API_KEY="$(cat "${config.age.secrets.gemini_api_key.path}")"
-            fi
-            if [ -f "${config.age.secrets.openrouter_api_key.path}" ]; then
-              export OPENROUTER_API_KEY="$(cat "${config.age.secrets.openrouter_api_key.path}")"
-            fi
-          '';
         };
       };
     };
