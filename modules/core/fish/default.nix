474af3df6e948fccb1d726907f1ec398695653df
annmaro <anandk60440@gmail.com>
2026-09-15 13:06:47 +0530

    added agenix abbr

--- modules/core/fish/default.nix
+++ modules/core/fish/default.nix
@@ -11,18 +11,18 @@
 
         programs.fish = {
           enable = true;
-          
+
           interactiveShellInit = ''
             # Key Bindings
             bind \ca beginning-of-line
             bind \ce end-of-line
-            
+
             # Environment variables
             set -q XDG_CONFIG_HOME; and set -gx XMONAD_CONFIG_DIR "$XDG_CONFIG_HOME/xmonad"; or set -gx XMONAD_CONFIG_DIR "$HOME/.config/xmonad"
             set -q XDG_DATA_HOME; and set -gx XMONAD_DATA_DIR "$XDG_DATA_HOME/xmonad"; or set -gx XMONAD_DATA_DIR "$HOME/.local/share/xmonad"
             set -q XDG_CACHE_HOME; and set -gx XMONAD_CACHE_DIR "$XDG_CACHE_HOME/xmonad"; or set -gx XMONAD_CACHE_DIR "$HOME/.cache/xmonad"
             set -gx FZF_DEFAULT_OPTS " --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
-            
+
             # Override autosuggestion color to green
             set -g fish_color_autosuggestion green
 
@@ -105,7 +105,7 @@
             dr = "nixos-rebuild dry-run --flake .#default";
             ncb = "sudo nix-collect-garbage -d";
             ncg = "sudo nix-collect-garbage -d; and sudo /run/current-system/bin/switch-to-configuration boot";
-            vault-edit = "env SOPS_AGE_KEY_FILE=$HOME/.config/sops/age/keys.txt ${pkgs.sops}/bin/sops $HOME/my-nixos-flake/secrets/secrets.yaml";
+            agenix = "agenix -i ~/.config/agenix/key.txt";
 
             dots = "cd ~/nixri/";
             games = "cd /mnt/games/";
