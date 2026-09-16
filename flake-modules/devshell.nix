92a148efae837b40bbf6100ee53f8810e73f0b1f
annmaro <anandk60440@gmail.com>
2026-09-16 13:45:21 +0530

    created rust & python devshell config

--- flake-modules/devshell.nix
+++ flake-modules/devshell.nix
@@ -1,5 +1,34 @@
 { ... }:
 
 {
-  # Development shell outputs can be added here as the project tooling settles.
+  perSystem = { pkgs, ... }: {
+    devShells = {
+      rust = pkgs.mkShell {
+        name = "rust-devshell";
+
+        packages = with pkgs; [
+          cargo
+          clippy
+          rust-analyzer
+          rustc
+          rustfmt
+        ];
+      };
+
+      python = pkgs.mkShell {
+        name = "python-devshell";
+
+        packages = with pkgs; [
+          (python3.withPackages (pythonPackages: with pythonPackages; [
+            pip
+            setuptools
+            wheel
+          ]))
+          pyright
+          ruff
+          uv
+        ];
+      };
+    };
+  };
 }
