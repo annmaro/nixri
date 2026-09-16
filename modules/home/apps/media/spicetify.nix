6bc898255f211d09c3326eac8187322b92ba60e0
annmaro <anandk60440@gmail.com>
2026-09-16 23:46:08 +0530

    updated theme to comfy sunset

--- modules/home/apps/media/spicetify.nix
+++ modules/home/apps/media/spicetify.nix
@@ -11,7 +11,21 @@ in
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.comfy;
+    theme = {
+      name = "Comfy";
+      src = pkgs.fetchFromGitHub {
+        owner = "Comfy-Themes";
+        repo = "Spicetify";
+        rev = "32ff101e27cfd33d85b7cc587f7f95db6b2df8b0";
+        hash = "sha256-HxfBiDNGI41u1KK4Q6YdRrOylD2frNGDBx2jTanEaQs=";
+      };
+      injectCss = true;
+      injectThemeJs = true;
+      replaceColors = true;
+      overwriteAssets = true;
+      sidebarConfig = true;
+      homeConfig = true;
+    };
     colorScheme = "Sunset";
     enabledExtensions = with spicePkgs.extensions; [
       adblock
