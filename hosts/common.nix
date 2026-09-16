f04a78eb4299b9ee226352114b7eec5fd2d8284e
annmaro <anandk60440@gmail.com>
2026-09-17 04:22:01 +0530

    added greetd

--- hosts/common.nix
+++ hosts/common.nix
@@ -6,7 +6,7 @@
   imports = [
     ../modules/core/settings.nix
     ../modules/nixos/desktop/niri.nix
-    ../modules/nixos/desktop/sddm.nix
+    ../modules/nixos/desktop/greetd.nix
     (inputs.import-tree ../modules/nixos/core)
     (inputs.import-tree ../modules/nixos/hardware)
     (inputs.import-tree ../modules/nixos/services)
