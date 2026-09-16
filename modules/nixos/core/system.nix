b2f6ac633c9c5763d271dff5fbaaefdee31496d5
annmaro <anandk60440@gmail.com>
2026-09-16 14:13:19 +0530

    added the nixConfig cache

--- modules/nixos/core/system.nix
+++ modules/nixos/core/system.nix
@@ -36,6 +36,7 @@
       keep-derivations = true;
       substituters = [
         "https://cache.nixos.org/"
+        "https://niri.cachix.org/"
         "https://nix-community.cachix.org/"
         "https://chaotic-nyx.cachix.org/"
         "https://cachix.cachix.org"
@@ -43,6 +44,7 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
