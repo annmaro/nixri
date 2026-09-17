8f8c11926268cd42de2b3bc071d70c2f4552c089
annmaro <anandk60440@gmail.com>
2026-09-17 07:12:14 +0530

    removed rregreet and added qtgreet

--- modules/nixos/desktop/greetd.nix
+++ modules/nixos/desktop/greetd.nix
@@ -2,34 +2,75 @@
 
 let
   wallpaper = "${config.users.users.${config.systemSettings.username}.home}/Pictures/Wallpapers/output_1080p.mp4";
-  greeterWallpaper = "/etc/greetd/wallpaper.mp4";
-  greeterSwayConfig = pkgs.writeText "greetd-sway.conf" ''
+  qtgreetTheme = "qtgreet/themes/nixri-video";
+  sessionDesktops = "${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+  greeterSwayConfig = pkgs.writeText "qtgreet-sway.conf" ''
     default_border none
     default_floating_border none
-    output * bg #000000 solid_color
-    exec ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio --loop-file=inf --cache=no --demuxer-readahead-secs=1 --hwdec=auto" '*' '${greeterWallpaper}'
-    exec ${pkgs.regreet}/bin/regreet
+    exec ${pkgs.qtgreet}/bin/qtgreet \
+      --config /etc/qtgreet/config.ini \
+      --data-path /var/lib/qtgreet \
+      --wl-session-path ${sessionDesktops}
   '';
   greeterCommand = pkgs.writeShellScript "greetd-session" ''
-    exec ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --config ${greeterSwayConfig}
+    exec ${pkgs.dbus}/bin/dbus-run-session \
+      ${pkgs.sway}/bin/sway --config ${greeterSwayConfig}
   '';
 in
 {
   environment.etc."greetd/wallpaper.mp4".source = wallpaper;
 
+  environment.etc."qtgreet/config.ini".text = ''
+    [General]
+    Backend = GreetD
+    Theme = nixri-video
+    BlurBackground = false
+    IconTheme = breeze
+
+    [Overrides]
+    Background = Theme
+    BaseColor = Theme
+    TextColor = Theme
+    FontFamily = Theme
+
+    [Environment]
+
+    [PowerCommands]
+    Suspend = dbus
+    Hibernate = dbus
+    Shutdown = dbus
+    Reboot = dbus
+  '';
+
+  environment.etc."${qtgreetTheme}/index.theme".text = ''
+    [General]
+    Name = Nixri Video
+
+    [Files]
+    Layout = layout.hjson
+    StyleSheet = style.qss
+
+    [Theme]
+    BaseColor = ffffffff
+    TextColor = ffffffff
+    Type = Video
+
+    [videobg]
+    File = /etc/greetd/wallpaper.mp4
+  '';
+
+  environment.etc."${qtgreetTheme}/layout.hjson".source =
+    "${pkgs.qtgreet}/share/qtgreet/themes/default/layout.hjson";
+  environment.etc."${qtgreetTheme}/style.qss".source =
+    "${pkgs.qtgreet}/share/qtgreet/themes/default/style.qss";
+
+  environment.variables.QTGREET_THEME_DIRS = "/etc/qtgreet/themes";
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/qtgreet 0755 greeter greeter - -"
+  ];
+
   services.displayManager.defaultSession = "niri";
-  services.displayManager.regreet = {
-    enable = true;
-    settings.skip_selection = true;
-    extraCss = ''
-      /* The centered frame is the login panel; the clock frame also has
-         the background class but is marked as a top overlay. */
-      overlay > frame.background:not(.top) {
-        margin-left: 40px;
-        margin-right: 540px;
-      }
-    '';
-  };
 
   services.greetd = {
     enable = true;
@@ -41,8 +82,7 @@ in
   };
 
   environment.systemPackages = with pkgs; [
-    mpvpaper
-    regreet
+    qtgreet
     sway
   ];
 }
