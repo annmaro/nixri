b9913cf6e763d5e87c03ee7172f654315c7e7cdb
annmaro <anandk60440@gmail.com>
2026-09-24 20:09:26 +0530

    updated config

--- modules/home/apps/browser/qutebrowser.nix
+++ modules/home/apps/browser/qutebrowser.nix
@@ -30,6 +30,7 @@ in
       c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
       c.content.canvas_reading = True
       c.content.webgl = True
+      c.content.cookies.accept = 'all'
 
       # --- General Settings ---
       c.url.start_pages = ["https://duckduckgo.com"]
