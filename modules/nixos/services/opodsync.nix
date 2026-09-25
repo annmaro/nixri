846f1eb3adb3d77ea54b5a78e824cdc02735062e
annmaro <anandk60440@gmail.com>
2026-09-26 03:40:12 +0530

    updated port config

--- modules/nixos/services/opodsync.nix
+++ modules/nixos/services/opodsync.nix
@@ -3,12 +3,12 @@
 {
   virtualisation.oci-containers.containers.opodsync = {
     image = "ganeshlab/opodsync:latest";
-    ports = [ "8080:8080" ];
+    ports = [ "8081:8080" ];
     volumes = [
       "/var/lib/opodsync:/var/www/html/data"
     ];
   };
 
-  # Open port 8080 for oPodSync
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  # Open port 8081 for oPodSync
+  networking.firewall.allowedTCPPorts = [ 8081 ];
 }
