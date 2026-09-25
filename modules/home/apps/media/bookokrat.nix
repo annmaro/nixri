03ad5af88a285d9363f6cf6cf37c6d4d22284f71
annmaro <anandk60440@gmail.com>
2026-09-26 04:16:40 +0530

    added zoom keymaps

--- modules/home/apps/media/bookokrat.nix
+++ modules/home/apps/media/bookokrat.nix
@@ -4,4 +4,11 @@
   home.packages = [
     inputs.bookokrat.packages.x86_64-linux.default
   ];
+
+  xdg.configFile."bookokrat/keybindings.toml".text = ''
+    [content]
+    "+" = "zoom_in"
+    "-" = "zoom_out"
+    "=" = "reset_zoom"
+  '';
 }
