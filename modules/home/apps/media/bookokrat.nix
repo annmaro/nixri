55132441b4cf381e65a1f83bc46e6096eae33608
annmaro <anandk60440@gmail.com>
2026-09-26 04:48:36 +0530

    removed zoom config as they were not needed

--- modules/home/apps/media/bookokrat.nix
+++ modules/home/apps/media/bookokrat.nix
@@ -4,11 +4,4 @@
   home.packages = [
     inputs.bookokrat.packages.x86_64-linux.default
   ];
-
-  xdg.configFile."bookokrat/keybindings.toml".text = ''
-    [content]
-    "+" = "zoom_in"
-    "-" = "zoom_out"
-    "=" = "reset_zoom"
-  '';
 }
